f1f7bf6db7d46912333fdce97ce4f36073241cad
tleeaf <thomas.lee@uconn.edu>
2025-05-20 15:28:02 -0400

    Placeholder video title

--- components/video.vue
+++ components/video.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="my-24 mx-auto justify-center flex"> <video src="https://videos.pexels.com/video-files/3129576/3129576-uhd_2560_1440_30fps.mp4" autoplay loop
+  <div class="my-24 mx-auto justify-center flex">
+     <!-- <video src="https://videos.pexels.com/video-files/3129576/3129576-uhd_2560_1440_30fps.mp4" autoplay loop
       muted class="bg-gray-500 md:rounded-4xl w-1/2 h-1/3 md:w-5/6 md:h-200 mx-4 lg:mx-36 md:my-24">
       <source src="https://videos.pexels.com/video-files/3129576/3129576-uhd_2560_1440_30fps.mp4" type="video/mp4">
       <source src="https://videos.pexels.com/video-files/3129576/3129576-uhd_2560_1440_30fps.webm" type="video/webm">
       Your browser does not support the video tag.
-    </video>
+    </video> -->
+    <div class="bg-gray-500 md:rounded-4xl w-1/2 h-1/3 md:w-5/6 md:h-200 mx-4 lg:mx-36 md:my-24 grid place-content-center">
+      <p class="text-5xl font-bold text-white">Centennial Welcome Intro Animation</p>
+    </div>
   </div>
 </template>
 
