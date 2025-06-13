fc2c2adeeb4578974f0c5e5d4d30eb1cbc7282be
tleeaf <thomas.lee@uconn.edu>
2025-06-13 17:30:41 -0400

    first time modal for anim, filtering

--- components/timeline.vue
+++ components/timeline.vue
@@ -3,48 +3,17 @@
     <h1 class="text-blue-950 text-5xl md:text-9xl font-black timeline-title dark:text-blue-300">OUR TIMELINE</h1>
     <section>
       <Filters />
-      <select class="select select-bordered w-full max-w-xs mt-4 border-2 border-black ml-24 my-5" v-model="selectedLayoutMode">
-        <option v-for="mode in layoutModes" :key="mode.label" :value="mode">{{ mode.label }}</option>
-      </select>
-    </section>
-
-    <!-- Card Grid Layout (Flex)-->
-    <section class="flex justify-between" v-if="selectedLayoutMode.label === 'Flex'">
-      <ul class="mx-auto flex md:flex-row flex-col gap-8 justify-between" v-auto-animate>
-        <li class="flex flex-col sm:w-full md:w-1/4 gap-5 justify-start" v-for="(j, index) in postGroups" :key="index">
-          <Card v-for="i in j" :post="i" :x-multiplier="1" :y-multiplier="1" mode="autoHeight" />
-        </li>
-      </ul>
     </section>
 
     <!-- Card Layout (CSS Grid) -->
-    <section class="flex justify-around" v-else-if="selectedLayoutMode.label === 'Grid'">
+    <section class="flex justify-around">
       <ul class="mx-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 md:gap-8" id="card-grid" v-auto-animate>
-        <li class="grid-item mx-auto" v-for="(j, index) in filteredPosts" :key="index"
-          :class="index % 7 == 2 ? 'md:col-span-2' : ''">
-          <Card :post="j" :x-multiplier="index % 6 == 1 ? 2 : 1" :y-multiplier="1" mode="fixedHeight"/>
+        <li class="grid-item mx-auto w-full" v-for="(j, index) in filteredPosts" :key="index"
+          :class="index % 5 == 0 ? 'md:col-span-2' : ''">
+          <Card :post="j" :x-multiplier="index % 5 == 1 ? 2 : 1" :y-multiplier="1" mode="fixedHeight"/>
         </li>
       </ul>
     </section>
-
-    <!-- <section class="flex justify-between " >
-      <ul ref="gridContainer" class="grid-stack w-full">
-        <li v-for="(post, index) in postsArray" :key="index" :gs-w="3" :gs-h="4" :gs-x="(index)%12"
-        :gs-y="Math.floor((index) / 12)" class="grid-stack-item">
-          <Card :post="post" class="m-4"  />
-        </li>
-      </ul>
-    </section> -->
-
-    <!-- <section class="grid gap-8 grid-cols-4 auto-rows-auto">
-      <Card v-for="card in posts" :post="card" :key="card.slug"></Card>
-    </section> -->
-
-    <!-- <section class="flex flex-wrap mx-auto">
-      <div v-for="card in posts" class="p-2 inline w-1/4" :key="card">
-        <Card :post="card" />
-      </div>
-    </section> -->
 
     <!-- <section class="my-5 md:px-18 px-2">
       <div class="grid md:grid-cols-2 grid-cols-1 mx-auto gap-8">
@@ -64,6 +33,7 @@ import { GridStack } from 'gridstack'
 import 'gridstack/dist/gridstack.min.css';
 import Masonry from 'masonry-layout';
 import imagesLoaded from 'imagesloaded';
+import { useStore } from '~/stores/store';
 
 const gridContainer = useTemplateRef('gridContainer');
 const store = useStore();
@@ -93,54 +63,32 @@ const postsArray = computed(() => {
   }
   return [] as Post[];
 });
-const filteredPosts = computed(() => {
-  return postsArray.value.filter((post: Post) => {
-    return store.searchTerm.length > 0 ? post.title.toLowerCase().includes(store.searchTerm.toLowerCase()) : true;
-  });
+
+const postsFilteredByCategory = computed(() => {
+  if (store.timelineFilterCategories.length > 0) {
+    return postsArray.value.filter((post: Post) => {
+      return post.categories.nodes.some((cat: Category) => 
+        store.timelineFilterCategories.some(c => c.slug === cat.slug)
+      );w
+    });
+  }
+  return postsArray.value;
 });
-const postGroups = computed(() => {
-  const n = numCols.value;
-  return store.timelineFilterCategories.length > 0 ? filteredPosts.value.filter((post: Post) => {
-    return post.categories.nodes.some((category: Category) =>
-      store.timelineFilterCategories.map(c => c.slug).includes(category.slug)
-    );
-  }) : filteredPosts.value.reduce((acc, item, i) => {
-    acc[i % n].push(item);
-    return acc;
-  }, Array.from({ length: n }, () => []));
+
+const filteredPosts = computed(() => {
+  return postsFilteredByCategory.value.filter((post: Post) => {
+    return store.searchTerm.length > 0 ? post.title.toLowerCase().includes(store.searchTerm.toLowerCase()) : postsFilteredByCategory.value;
+  });
 });
 
 onMounted(async () => {
   anime({ targets: '.timeline-title', translateX: [-200, 0], duration: 700 });
   updateColumns()
   window.addEventListener('resize', updateColumns)
-  // imagesLoaded('#card-grid', () => {
-  //   new Masonry('#card-grid', {
-  //     gutter: 20,
-  //     columnWidth: 300,
-  //     itemSelector: '.grid-item',
-  //   });
-  // });
 onUnmounted(() => {
   window.removeEventListener('resize', updateColumns)
 })
 });
-
-// watch(postsArray, async (newPosts) => {
-//   if (newPosts.length > 0) {
-//     await nextTick(); // let Vue render all grid items
-
-//     console.log('GridStack items:', gridContainer.value?.querySelectorAll('.grid-stack-item'));
-
-//     GridStack.init({
-//       column: 12,
-//       minRow: 10,
-//       sizeToContent: true,
-//       // cellHeight: '25px',
-//       margin: 10,
-//     });
-//   }
-// });
 </script>
 
 <style>
