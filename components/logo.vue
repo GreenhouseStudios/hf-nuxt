41374e5d6ea50e4c5dfdbf4dac0dc4191c15cf83
Joel Salisbury <joel@uconn.edu>
2025-12-29 13:49:27 -0500

    removed extraneous firstload logic from the logo

--- components/logo.vue
+++ components/logo.vue
@@ -1,50 +1,15 @@
 <template>
-    <NuxtLink to="https://greaterhartfordgives.org/" target="_blank" style="z-index: 1;" class="fade-in img-transition relative" id="nav-img">
+    <NuxtLink to="https://greaterhartfordgives.org/" target="_blank" style="z-index: 1;" class="fade-in relative" id="nav-img">
         <img
           src="~/public/GHGF_Logo_RGB_Primary.png"
           alt="Logo"
-          ref="logoEl"
-          class="object-contain max-h-14 md:h-14 "
-
+          class="object-contain max-h-14 md:h-14"
         />
       <h3 class="site-link text-sm mt-1.5 text-cetacean font-medium text-center px-2 py-1 absolute" >GreaterHartfordGives.org</h3>
     </NuxtLink>
 </template>
 
 <script setup lang="ts">
-import {computed, onMounted, ref, watch, nextTick } from "vue";
-import { useFirstVisit } from "~/composables/useFirstVisit";
-
-const { ready, hasSeen } = useFirstVisit();
-const shouldClip = computed(() => ready.value && !hasSeen.value && window.innerWidth > 1150);
-
-const logoEl = ref<HTMLImageElement | null>(null);
-
-watch(
-    () => ready.value,
-    async (isReady) => {
-      if (!isReady) return
-      console.log('seen');
-      console.log(hasSeen)
-      await nextTick()
-      setClip()
-    },
-    { immediate: true }
-)
-const width = ref(window.innerWidth);
-onMounted(() => {
-  width.value = window.innerWidth
-})
-
-
-
-function setClip() {
-  if(!logoEl.value) return;
-  if(shouldClip.value) {
-    logoEl.value.classList.add('clipped');
-  }
-}
-
 </script>
 
 <style scoped>
@@ -56,16 +21,6 @@ function setClip() {
 @keyframes fade-in {
   from { opacity: 0 }
   to { opacity: 1 }
-}
-
-.img-transition {
-  transition: clip-path .75s ease-in-out;
-}
-.partial-clip {
-  clip-path: inset(0% 0% 0% 21%);
-}
-.clipped {
-  clip-path: inset(0% 0% 0% 100%) !important;
 }
 
 #nav-img:hover .site-link {
